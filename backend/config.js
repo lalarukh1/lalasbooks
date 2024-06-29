bb78e610cdba69368a9fa422a4192f616ca96c43
lalarukh1 <35193027+lalarukh1@users.noreply.github.com>
2024-06-29 15:56:03 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = ;
 
-export const mongoDBURL = "mongodb+srv://lalarukh1992:OEIZNTm3@bookstore.5c2md8o.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore";
+export const mongoDBURL = "";
